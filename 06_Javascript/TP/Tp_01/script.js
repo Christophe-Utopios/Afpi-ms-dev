d3e5f18139442a61e02ec63532e3190fe16c8f7f
Christophe-Utopios <christophe.r@utopios.solutions>
2024-01-03 02:18:32 -0800

    structure Tp01

---
+++ 06_Javascript/TP/Tp_01/script.js
@@ -0,0 +1,42 @@
+
+// Déclaration des variables
+let nbUser,
+    nbMystere,
+    nbCoups,
+    gagne
+
+// Récupération des éléments du DOM
+const validerBtn = document.querySelector("#validerBtn")
+const rejouerBtn = document.querySelector("#rejouerBtn")
+const ligne1 = document.querySelector("#ligne1")
+const ligne2 = document.querySelector("#ligne2")
+const nbCoupsHtml = document.querySelector("#nbCoups")
+const nbUserHtml = document.querySelector("#nbUser")
+
+
+function StartGame() {
+    // Génération d'un nombre aléatoire
+
+
+    // Initialiser des champs HTML et réinitialiser nos variables
+}
+
+// Fonction pour actualiser le nbCoups (Jeu + HTML)
+function UpdateNbCoups() {
+
+}
+
+// Fonction déclenchée par le bouton valider
+function Valider(){
+
+}
+
+// Logique lors de la fin de la partie
+function EndGame(){
+    
+}
+
+// Débloque le jeu pour redémarrer une partie
+function Rejouer() {
+
+}

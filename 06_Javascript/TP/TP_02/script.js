32826e2bb6c6a6e8023469009bd4f3cbec6f9c36
Christophe-Utopios <christophe.r@utopios.solutions>
2024-01-04 02:08:01 -0800

    structure tp02

---
+++ 06_Javascript/TP/TP_02/script.js
@@ -0,0 +1,12 @@
+// ... => spread operator = copie d'un tableau
+const touches = [...document.querySelectorAll(".bouton")]
+const ecranHaut = document.querySelector(".ecranHaut")
+const ecranBas = document.querySelector(".ecranBas")
+
+// Recupération de la data-key au clique d'un bouton.
+
+
+// Fonction permettant d'effectuer les actions des touches.
+function Calculer() {
+    
+}
